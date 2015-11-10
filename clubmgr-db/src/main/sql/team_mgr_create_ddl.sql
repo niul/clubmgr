7c1cf6d88e00301683bc89f9149adc7d3c4f88a8
Niul Bird <niul.bird@gmail.com>
2015-11-10 20:37:18 +0000

    Added Maven

--- clubmgr-db/src/main/sql/team_mgr_create_ddl.sql
+++ clubmgr-db/src/main/sql/team_mgr_create_ddl.sql
@@ -1,85 +1,85 @@
 /** team_mgr drop schema */
 
 CREATE TABLE clubs (
-	club_id		serial PRIMARY KEY,
-	name		varchar(40) NOT NULL,
-	club_key	varchar(40) NOT NULL,
-	created		timestamp
+    club_id        serial PRIMARY KEY,
+    name        varchar(40) NOT NULL,
+    club_key    varchar(40) NOT NULL,
+    created        timestamp
 );
 
 CREATE TABLE teams (
-	team_id		serial PRIMARY KEY,
-	club_id		serial,
-	name		varchar(40) NOT NULL,
-	team_key	varchar(40) NOT NULL,
-	created		timestamp,
-	CONSTRAINT FK_teams_clubs_id FOREIGN KEY (club_id)
-	REFERENCES clubs (club_id) MATCH SIMPLE
-	ON UPDATE CASCADE ON DELETE CASCADE
+    team_id        serial PRIMARY KEY,
+    club_id        serial,
+    name        varchar(40) NOT NULL,
+    team_key    varchar(40) NOT NULL,
+    created        timestamp,
+    CONSTRAINT FK_teams_clubs_id FOREIGN KEY (club_id)
+    REFERENCES clubs (club_id) MATCH SIMPLE
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE seasons (
-	season_id	serial PRIMARY KEY,
-	name		varchar(40) NOT NULL,
-	season_key	varchar(40) NOT NULL,
-	created		timestamp
+    season_id    serial PRIMARY KEY,
+    name        varchar(40) NOT NULL,
+    season_key    varchar(40) NOT NULL,
+    created        timestamp
 );
 
 CREATE TABLE fixtures (
-	fixture_id	serial PRIMARY KEY,
-	team_id		serial,
-	season_id	serial,
-	home		VARCHAR(40) NOT NULL,
-	away		VARCHAR(40) NOT NULL,
-	home_score	VARCHAR(10),
-	away_score	VARCHAR(10),
-	field		VARCHAR(80),
-	field_map_uri	VARCHAR(80),
-	date		date,
-	time		time,
-	created		timestamp,
-	CONSTRAINT FK_fixtures_team_id FOREIGN KEY (team_id)
-	REFERENCES teams (team_id) MATCH SIMPLE
-	ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT FK_fixtures_season_id FOREIGN KEY (season_id)
-	REFERENCES seasons (season_id) MATCH SIMPLE
-	ON UPDATE CASCADE ON DELETE CASCADE
+    fixture_id    serial PRIMARY KEY,
+    team_id        serial,
+    season_id    serial,
+    home        VARCHAR(40) NOT NULL,
+    away        VARCHAR(40) NOT NULL,
+    home_score    VARCHAR(10),
+    away_score    VARCHAR(10),
+    field        VARCHAR(80),
+    field_map_uri    VARCHAR(80),
+    date        date,
+    time        time,
+    created        timestamp,
+    CONSTRAINT FK_fixtures_team_id FOREIGN KEY (team_id)
+    REFERENCES teams (team_id) MATCH SIMPLE
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT FK_fixtures_season_id FOREIGN KEY (season_id)
+    REFERENCES seasons (season_id) MATCH SIMPLE
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE standings (
-	standing_id	serial PRIMARY KEY,
-	team_id		serial,
-	season_id	serial,
-	position	int,
-	team		VARCHAR(40) NOT NULL,
-	gamesPlayed	int NOT NULL,
-	wins		int NOT NULL,
-	ties		int NOT NULL,
-	losses		int NOT NULL,
-	goalsFor	int NOT NULL,
-	goalsAgainst	int NOT NULL,
-	points		int NOT NULL,
-	created		timestamp,
-	CONSTRAINT FK_standings_teams_id FOREIGN KEY (team_id)
-	REFERENCES teams (team_id) MATCH SIMPLE
-	ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT FK_standings_season_id FOREIGN KEY (season_id)
-	REFERENCES seasons (season_id) MATCH SIMPLE
-	ON UPDATE CASCADE ON DELETE CASCADE
+    standing_id    serial PRIMARY KEY,
+    team_id        serial,
+    season_id    serial,
+    position    int,
+    team        VARCHAR(40) NOT NULL,
+    gamesPlayed    int NOT NULL,
+    wins        int NOT NULL,
+    ties        int NOT NULL,
+    losses        int NOT NULL,
+    goalsFor    int NOT NULL,
+    goalsAgainst    int NOT NULL,
+    points        int NOT NULL,
+    created        timestamp,
+    CONSTRAINT FK_standings_teams_id FOREIGN KEY (team_id)
+    REFERENCES teams (team_id) MATCH SIMPLE
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT FK_standings_season_id FOREIGN KEY (season_id)
+    REFERENCES seasons (season_id) MATCH SIMPLE
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE team_season_map (
-	team_season_map_id	serial PRIMARY KEY,
-	team_id			serial,
-	season_id		serial,
-	data_key		varchar(20) NOT NULL,
-	fixtures_uri		varchar(80) NOT NULL,
-	standings_uri		varchar(80) NOT NULL,
-	created			timestamp,
-	CONSTRAINT FK_team_season_map_teams_id FOREIGN KEY (team_id)
-	REFERENCES teams (team_id) MATCH SIMPLE
-	ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT FK_team_season_map_season_id FOREIGN KEY (season_id)
-	REFERENCES seasons (season_id) MATCH SIMPLE
-	ON UPDATE CASCADE ON DELETE CASCADE
+    team_season_map_id    serial PRIMARY KEY,
+    team_id            serial,
+    season_id        serial,
+    data_key        varchar(20) NOT NULL,
+    fixtures_uri        varchar(80) NOT NULL,
+    standings_uri        varchar(80) NOT NULL,
+    created            timestamp,
+    CONSTRAINT FK_team_season_map_teams_id FOREIGN KEY (team_id)
+    REFERENCES teams (team_id) MATCH SIMPLE
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT FK_team_season_map_season_id FOREIGN KEY (season_id)
+    REFERENCES seasons (season_id) MATCH SIMPLE
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
--- clubmgr-db/src/main/sql/team_mgr_create_dml.sql
+++ clubmgr-db/src/main/sql/team_mgr_create_dml.sql
@@ -3,33 +3,33 @@
 
 /** CLUBS */
 INSERT INTO clubs (name, club_key, created)
-VALUES	('Bombastic FC', 'BOMBASTIC', CURRENT_TIMESTAMP);
+VALUES    ('Bombastic FC', 'BOMBASTIC', CURRENT_TIMESTAMP);
 
 
 /** TEAMS */
 INSERT INTO teams (club_id, name, team_key, created) (
-	SELECT	club_id, 'Mens A', 'BOMBASTIC_MENS_A', CURRENT_TIMESTAMP
-	FROM	clubs WHERE club_key = 'BOMBASTIC'
+    SELECT    club_id, 'Mens A', 'BOMBASTIC_MENS_A', CURRENT_TIMESTAMP
+    FROM    clubs WHERE club_key = 'BOMBASTIC'
 );
 
 INSERT INTO teams (club_id, name, team_key, created) (
-	SELECT	club_id, 'Mens B', 'BOMBASTIC_MENS_B', CURRENT_TIMESTAMP
-	FROM	clubs WHERE club_key = 'BOMBASTIC'
+    SELECT    club_id, 'Mens B', 'BOMBASTIC_MENS_B', CURRENT_TIMESTAMP
+    FROM    clubs WHERE club_key = 'BOMBASTIC'
 );
 
 INSERT INTO teams (club_id, name, team_key, created) (
-	SELECT	club_id, 'Mens Classics', 'BOMBASTIC_MENS_CLASSICS', CURRENT_TIMESTAMP
-	FROM	clubs WHERE club_key = 'BOMBASTIC'
+    SELECT    club_id, 'Mens Classics', 'BOMBASTIC_MENS_CLASSICS', CURRENT_TIMESTAMP
+    FROM    clubs WHERE club_key = 'BOMBASTIC'
 );
 
 INSERT INTO teams (club_id, name, team_key, created) (
-	SELECT	club_id, 'Mens Jurassic', 'BOMBASTIC_MENS_JURASSIC', CURRENT_TIMESTAMP
-	FROM	clubs WHERE club_key = 'BOMBASTIC'
+    SELECT    club_id, 'Mens Jurassic', 'BOMBASTIC_MENS_JURASSIC', CURRENT_TIMESTAMP
+    FROM    clubs WHERE club_key = 'BOMBASTIC'
 );
 
 INSERT INTO teams (club_id, name, team_key, created) (
-	SELECT	club_id, 'Womens Jurassic', 'BOMBASTIC_WOMENS_A', CURRENT_TIMESTAMP
-	FROM	clubs WHERE club_key = 'BOMBASTIC'
+    SELECT    club_id, 'Womens Jurassic', 'BOMBASTIC_WOMENS_A', CURRENT_TIMESTAMP
+    FROM    clubs WHERE club_key = 'BOMBASTIC'
 );
 
 INSERT INTO seasons (name, season_key, created)
@@ -38,120 +38,120 @@ VALUES ('Summer 2015', 'SUMMER_2015', CURRENT_TIMESTAMP);
 INSERT INTO seasons (name, season_key, created)
 VALUES ('Winter 2015/2016', 'WINTER_2015', CURRENT_TIMESTAMP);
 INSERT INTO standings (team_id, season_id, position, team, gamesPlayed, wins, ties, losses, goalsFor, goalsAgainst, points, created) (
-	SELECT	team_id, season_id, 1, 'Rinos Fury A', 7, 6, 1, 0, 15, 6, 19, CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_A'
+    SELECT    team_id, season_id, 1, 'Rinos Fury A', 7, 6, 1, 0, 15, 6, 19, CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_A'
 );
 
 INSERT INTO standings (team_id, season_id, position, team, gamesPlayed, wins, ties, losses, goalsFor, goalsAgainst, points, created) (
-	SELECT	team_id, season_id, 2, 'Gastown FC', 7, 4, 1, 2, 14, 12, 13, CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_A'
+    SELECT    team_id, season_id, 2, 'Gastown FC', 7, 4, 1, 2, 14, 12, 13, CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_A'
 );
 
 INSERT INTO standings (team_id, season_id, position, team, gamesPlayed, wins, ties, losses, goalsFor, goalsAgainst, points, created) (
-	SELECT	team_id, season_id, 3, 'Vancouver Strikers', 6, 4, 1, 1, 12, 7, 13, CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_A'
+    SELECT    team_id, season_id, 3, 'Vancouver Strikers', 6, 4, 1, 1, 12, 7, 13, CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_A'
 );
 
 INSERT INTO standings (team_id, season_id, position, team, gamesPlayed, wins, ties, losses, goalsFor, goalsAgainst, points, created) (
-	SELECT	team_id, season_id, 4, 'HK Hrvat A', 7, 4, 0, 3, 12, 10, 12, CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_A'
+    SELECT    team_id, season_id, 4, 'HK Hrvat A', 7, 4, 0, 3, 12, 10, 12, CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_A'
 );
 
 INSERT INTO standings (team_id, season_id, position, team, gamesPlayed, wins, ties, losses, goalsFor, goalsAgainst, points, created) (
-	SELECT	team_id, season_id, 5, 'Harps FC A', 7, 3, 2, 2, 13, 9, 11, CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_A'
+    SELECT    team_id, season_id, 5, 'Harps FC A', 7, 3, 2, 2, 13, 9, 11, CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_A'
 );
 
 INSERT INTO standings (team_id, season_id, position, team, gamesPlayed, wins, ties, losses, goalsFor, goalsAgainst, points, created) (
-	SELECT	team_id, season_id, 6, 'Bombastic FC A', 7, 3, 1, 3, 11, 11, 10, CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_A'
+    SELECT    team_id, season_id, 6, 'Bombastic FC A', 7, 3, 1, 3, 11, 11, 10, CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_A'
 );
 
 INSERT INTO standings (team_id, season_id, position, team, gamesPlayed, wins, ties, losses, goalsFor, goalsAgainst, points, created) (
-	SELECT	team_id, season_id, 7, 'Squamish FC', 6, 3, 1, 2, 19, 9, 10, CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_A'
+    SELECT    team_id, season_id, 7, 'Squamish FC', 6, 3, 1, 2, 19, 9, 10, CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_A'
 );
 
 INSERT INTO standings (team_id, season_id, position, team, gamesPlayed, wins, ties, losses, goalsFor, goalsAgainst, points, created) (
-	SELECT	team_id, season_id, 8, 'Euro FC', 7, 2, 2, 3, 6, 9, 8, CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_A'
+    SELECT    team_id, season_id, 8, 'Euro FC', 7, 2, 2, 3, 6, 9, 8, CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_A'
 );
 
 INSERT INTO standings (team_id, season_id, position, team, gamesPlayed, wins, ties, losses, goalsFor, goalsAgainst, points, created) (
-	SELECT	team_id, season_id, 9, 'Vancouver United FC', 7, 2, 2, 3, 11, 13, 8, CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_A'
+    SELECT    team_id, season_id, 9, 'Vancouver United FC', 7, 2, 2, 3, 11, 13, 8, CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_A'
 );
 
 INSERT INTO standings (team_id, season_id, position, team, gamesPlayed, wins, ties, losses, goalsFor, goalsAgainst, points, created) (
-	SELECT	team_id, season_id, 10, '13th Legion FC', 7, 1, 3, 3, 6, 10, 6, CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_A'
+    SELECT    team_id, season_id, 10, '13th Legion FC', 7, 1, 3, 3, 6, 10, 6, CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_A'
 );
 
 INSERT INTO standings (team_id, season_id, position, team, gamesPlayed, wins, ties, losses, goalsFor, goalsAgainst, points, created) (
-	SELECT	team_id, season_id, 11, 'GFC United Lions', 7, 1, 1, 5, 8, 21, 4, CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_A'
+    SELECT    team_id, season_id, 11, 'GFC United Lions', 7, 1, 1, 5, 8, 21, 4, CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_A'
 );
 
 INSERT INTO standings (team_id, season_id, position, team, gamesPlayed, wins, ties, losses, goalsFor, goalsAgainst, points, created) (
-	SELECT	team_id, season_id, 12, 'America FC', 7, 0, 1, 6, 6, 16, 1, CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_A'
+    SELECT    team_id, season_id, 12, 'America FC', 7, 0, 1, 6, 6, 16, 1, CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_A'
 );
 
 INSERT INTO team_season_map (team_id, season_id, data_key, standings_uri, fixtures_uri, created) (
-	SELECT	team_id, season_id, 'VMSL', 'http://vmslsoccer.com/division-2b/', 'http://vmslsoccer.com/division-2b/', CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_A'
+    SELECT    team_id, season_id, 'VMSL', 'http://vmslsoccer.com/division-2b/', 'http://vmslsoccer.com/division-2b/', CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_A'
 );
 
 INSERT INTO team_season_map (team_id, season_id, data_key, standings_uri, fixtures_uri, created) (
-	SELECT	team_id, season_id, 'VMSL', 'http://vmslsoccer.com/cat-division/', 'http://vmslsoccer.com/cat-division/', CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_B'
+    SELECT    team_id, season_id, 'VMSL', 'http://vmslsoccer.com/cat-division/', 'http://vmslsoccer.com/cat-division/', CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_B'
 );
 
 INSERT INTO team_season_map (team_id, season_id, data_key, standings_uri, fixtures_uri, created) (
-	SELECT	team_id, season_id, 'VMSL', 'http://vmslsoccer.com/masters-division-1/', 'http://vmslsoccer.com/masters-division-1/', CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_CLASSICS'
+    SELECT    team_id, season_id, 'VMSL', 'http://vmslsoccer.com/masters-division-1/', 'http://vmslsoccer.com/masters-division-1/', CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_CLASSICS'
 );
 
 INSERT INTO team_season_map (team_id, season_id, data_key, standings_uri, fixtures_uri, created) (
-	SELECT	team_id, season_id, 'VMSL', 'http://vmslsoccer.com/masters-over-40s-division-1/', 'http://vmslsoccer.com/masters-over-40s-division-1/', CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_MENS_JURASSIC'
+    SELECT    team_id, season_id, 'VMSL', 'http://vmslsoccer.com/masters-over-40s-division-1/', 'http://vmslsoccer.com/masters-over-40s-division-1/', CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_MENS_JURASSIC'
 );
 
 INSERT INTO team_season_map (team_id, season_id, data_key, standings_uri, fixtures_uri, created) (
-	SELECT	team_id, season_id, 'MWSL', 'http://mwsl.com/webapps/spappz_live/team_page_mwsl?id=1920', 'http://mwsl.com/webapps/spappz_live/team_page_mwsl?id=1920', CURRENT_TIMESTAMP
-	FROM	teams T
-	JOIN	seasons S ON S.season_key = 'WINTER_2015'
-	WHERE	T.team_key = 'BOMBASTIC_WOMENS_A'
+    SELECT    team_id, season_id, 'MWSL', 'http://mwsl.com/webapps/spappz_live/team_page_mwsl?id=1920', 'http://mwsl.com/webapps/spappz_live/team_page_mwsl?id=1920', CURRENT_TIMESTAMP
+    FROM    teams T
+    JOIN    seasons S ON S.season_key = 'WINTER_2015'
+    WHERE    T.team_key = 'BOMBASTIC_WOMENS_A'
 );

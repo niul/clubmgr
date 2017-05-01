76025c5ee69948a3c93c6b92d58bac2a2db2b568
Niul Bird <niul@yahoo.com>
2017-05-01 13:04:52 -0700

    Added a String Trimmer for input parameters.

---
+++ clubmgr-bfc/src/main/java/com/niulbird/clubmgr/bfc/controller/ControllerSetup.java
@@ -0,0 +1,15 @@
+package com.niulbird.clubmgr.bfc.controller;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+public class ControllerSetup {
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        StringTrimmerEditor stringtrimmer = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class, stringtrimmer);
+    }
+}
